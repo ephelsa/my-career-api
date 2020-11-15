1dd65d56ddcabc99e429048861e86f6c639d5e5c
Leonardo A. Perez <xephelsax@gmail.com>
2020-11-15 14:30:36 -0500

    renamed app

--- cmd/mycareer/main.go
+++ cmd/mycareer/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	app2 "ephelsa/my-career/pkg/app"
+	"ephelsa/my-career/pkg/app"
 	"ephelsa/my-career/pkg/env"
 )
 
 func main() {
 	envConfig := env.Setup()
-	app2.StartApi(envConfig.Server)
+	app.StartApi(envConfig.Server)
 }
